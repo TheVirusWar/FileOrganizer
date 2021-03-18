569cfc59f11456279fe3c3fee8bf7304e8ee5dc4
BuildTools <unconfigured@null.spigotmc.org>
2021-03-18 17:53:39 +0100

    Comments.

--- FileOrganizer_v0.1/Program.cs
+++ FileOrganizer_v0.1/Program.cs
@@ -18,25 +18,29 @@ namespace FileOrganizer_v0._1
             {
                 return 1;
             }
-            oldName = fi.FullName;
-            int index = oldName.LastIndexOf('.');
+            //checks the name in case if its the app itself, so then it skips
+            //probably you can check for the app ITSELF, as an unique thing, so even if something else is named the same it still works how it should, but idk
 
-            string extenstion = oldName.Substring(index, oldName.Length - index);
+            oldName = fi.FullName; //full name with path
+            
+            int index = oldName.LastIndexOf('.'); //index is path till the . of the extension
 
-            index = oldName.LastIndexOf('\\');
-            Console.WriteLine(oldName);
-            string newName = oldName.Substring(0, index);
+            string extenstion = oldName.Substring(index, oldName.Length - index); //extension saved
 
-            newName = newName + "\\" + counter.ToString() + extenstion;
+            index = oldName.LastIndexOf('\\'); //changing index so its till \, so afterwards is only the files name
+
+            string newName = oldName.Substring(0, index); //new name is path (ye could just path), practice with strings
+
+            newName = newName + "\\" + counter.ToString() + extenstion; //path + \ + number + .txt
 
             File.Move(oldName, newName);
             return 0;
-            //there has to be a better way
         }
 
 
         static void Main(string[] args)
         {
+            //you sure?
             Console.WriteLine("ATTENTION! This program will rename EVERY FILE in the directory it is placed and name it from 0-x. BE WARNED! ");
             if(Console.ReadKey().KeyChar != 'y')
             {
@@ -50,10 +54,10 @@ namespace FileOrganizer_v0._1
             }
             Console.Clear();
 
-
-            DirectoryInfo di = new DirectoryInfo(@".\test");
+            DirectoryInfo di = new DirectoryInfo(@".");
             FileInfo[] rgFiles = di.GetFiles("*.*");
-            int counter = 0;
+            //gets all the files from the folder the app is in
+            int counter = 0; //naming
 
             Console.WriteLine("Old names:");
             foreach (FileInfo fi in rgFiles)
@@ -63,7 +67,7 @@ namespace FileOrganizer_v0._1
                 int check = Rename(fi, counter);
                 if (check == 0)
                 {
-                    counter++;
+                    counter++; //if the method returns 0, the counter gets updated, if not, it doesnt, which means its the app and it skips it
                 }
             }
 
@@ -72,7 +76,7 @@ namespace FileOrganizer_v0._1
             rgFiles = di.GetFiles("*.*");
             foreach (FileInfo fi in rgFiles)
             {
-                Console.WriteLine("Name: " + fi.Name + ", directory: " + fi.Directory);
+                Console.WriteLine("Name: " + fi.Name + ", directory: " + fi.Directory); //new names
             }
             Console.ReadLine();
         }
